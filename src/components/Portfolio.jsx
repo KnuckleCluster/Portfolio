97d149aaafe462897dca9083c96e5abe08962473
KnuckleCluster <kyleanarna@gmail.com>
2024-04-28 05:05:59 +0800

    minor revision

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,7 +13,7 @@ function Portfolio(){
                         <h1 className="text-center text-white text-[20px]">Austin</h1>
                     </div>
                     <div className="mr-[50px] group flex relative flex-col">
-                        <a href="https://austin-kappa.vercel.app/"><img src="img/library-app.png" alt="Library-App" className="w-[250px] h-[250px] rounded-[15px]"/></a>
+                        <a href="https://odin-library-eta.vercel.app/"><img src="img/library-app.png" alt="Library-App" className="w-[250px] h-[250px] rounded-[15px]"/></a>
                         <span class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 w-[300px] h-[110px] text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">A simple CRUD app in a form of library app. <br /> <br /> (Click here to visit the Library-app!)</span>
                         <h1 className="text-center text-white text-[20px]">Library-App</h1>
                     </div>
