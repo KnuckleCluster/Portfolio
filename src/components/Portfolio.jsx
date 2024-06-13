00e24589e184700f75fb04cc24b3b0321e0cd182
KnuckleCluster <kyleanarna@gmail.com>
2024-06-14 03:12:21 +0800

    updated projects tab

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,14 +8,19 @@ function Portfolio(){
                 <hr />
                 <div className="flex flex-row mt-[50px] justify-center">
                     <div className="mr-[50px] group flex relative flex-col">
-                        <a href="https://austin-kappa.vercel.app/"><img src="img/austin.png" alt="Austin" className="w-[250px] h-[250px] rounded-[15px]"/></a>
+                        <a href="https://austin-kappa.vercel.app/"><img src="img/austin.png" alt="Austin" className="w-[150px] h-[150px] rounded-[15px]"/></a>
                         <span class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 w-[250px] h-[130px] text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Austin is a fanmade Post Malone album site that lets you learn more about the album and play songs that are in the album. <br /> <br /> (Click here to visit Austin!)</span>
                         <h1 className="text-center text-white text-[20px]">Austin</h1>
                     </div>
                     <div className="mr-[50px] group flex relative flex-col">
-                        <a href="https://odin-library-eta.vercel.app/"><img src="img/library-app.png" alt="Library-App" className="w-[250px] h-[250px] rounded-[15px]"/></a>
+                        <a href="https://odin-library-eta.vercel.app/"><img src="img/library-app.png" alt="Library-App" className="w-[150px] h-[150px] rounded-[15px]"/></a>
                         <span class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 w-[300px] h-[110px] text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">A simple CRUD app in a form of library app. <br /> <br /> (Click here to visit the Library-app!)</span>
                         <h1 className="text-center text-white text-[20px]">Library-App</h1>
+                    </div>
+                    <div className="mr-[50px] group flex relative flex-col">
+                        <a href="https://todo-app-weld-delta.vercel.app/"><img src="img/todoapp.png" alt="Todo-App" className="w-[150px] h-[150px] rounded-[15px]"/></a>
+                        <span class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 w-[300px] h-[120px] text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">A CRUD app in a form of todo app. <br /> more complex than the library app, utilizes react states and localstorage <br /> (Click here to visit the Library-app!)</span>
+                        <h1 className="text-center text-white text-[20px]">Todo-App</h1>
                     </div>
 
                 </div>
